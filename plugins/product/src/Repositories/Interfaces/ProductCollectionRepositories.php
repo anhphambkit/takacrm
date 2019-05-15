f159a1030a5f19ffd05bc4bf29d50cf16cd698b3
AnhPhamBKIT <phamtuananh.bkit@gmail.com>
2019-05-16 06:27:18 +0700

    Revert "reinit"
    This reverts commit b1ce9d7d5d0ec1217f8af9edbcf47468f0e81820.

--- plugins/product/src/Repositories/Interfaces/ProductCollectionRepositories.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Plugins\Product\Repositories\Interfaces;
-use Core\Master\Repositories\Interfaces\RepositoryInterface;
-
-interface ProductCollectionRepositories extends RepositoryInterface{
-    
-}
